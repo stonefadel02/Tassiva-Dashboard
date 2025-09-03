4f3a8ead601ff92bda37f303c173eab237da98b1
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 <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>
2025-09-03 13:11:20 +0100

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,15 @@
-
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
-    <title>@yield('title', 'Tableau de bord')</title>
-        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap/bootstrap.css') }}">
+    <title>@yield('title', 'Tassiva Dashboard')</title>
+    <meta name="description" content="@yield('description', 'Sain avec les produits de chez nous')">
+
+    <meta property="og:title" content="@yield('title', 'Tassiva Dashboard')" />
+    <meta property="og:description" content="@yield('description', 'Sain avec les produits de chez nous)" />
+    <meta property="og:image" content="@yield('image', asset('assets/images/tassiva.jpg'))" />
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap/bootstrap.css') }}">
 
     <link rel="stylesheet" href="{{ asset('assets/css/backend-plugin.min.css') }}">
 
@@ -13,11 +18,13 @@
     <link rel="stylesheet" href="{{ asset('assets/css/loading.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
+
 <body>
-    @include('partials.navbar') 
+    @include('partials.navbar')
 
     <div class="container mt-5">
         @yield('content')
     </div>
 </body>
+
 </html>
