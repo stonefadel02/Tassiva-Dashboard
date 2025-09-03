613c2c7894a600c88f7661315ac928b8a79a07ff
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 <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>
2025-09-03 12:49:45 +0100

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
+use Illuminate\Support\Facades\URL; // Assure-toi que cette ligne est bien présente en haut du fichier
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -21,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+        if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+    }
     }
 }
