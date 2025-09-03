9cf48f43e631c3fc78fa7b229032ee04fc4bab7e
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 <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>
2025-09-03 13:19:45 +0100

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>@yield('title', 'Tassiva Dashboard')</title>
-    <meta name="description" content="@yield('description', 'Sain avec les produits de chez nous')">
-
-    <meta property="og:title" content="@yield('title', 'Tassiva Dashboard')" />
-    <meta property="og:description" content="@yield('description', 'Sain avec les produits de chez nous)" />
-    <meta property="og:image" content="@yield('image', asset('assets/images/tassiva.jpg'))" />
+    <title>@yield('title', 'Tableau de bord')</title>
+    
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap/bootstrap.css') }}">
 
     <link rel="stylesheet" href="{{ asset('assets/css/backend-plugin.min.css') }}">
