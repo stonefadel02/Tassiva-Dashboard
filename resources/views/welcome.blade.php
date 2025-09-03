687c5643a39ea2809e2834da6f2ef0eb0e5456c7
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 <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>
2025-09-03 11:51:49 +0100

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,6 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
